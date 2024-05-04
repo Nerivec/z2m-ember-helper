2785cce92d85b339889abd8b2ca6dba8034f265b
Nerivec <62446222+Nerivec@users.noreply.github.com>
2024-05-04 22:56:18 +0200

    Updates

--- src.77de5100.js
+++ src.77de5100.js
@@ -7754,7 +7754,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52943" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60579" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
